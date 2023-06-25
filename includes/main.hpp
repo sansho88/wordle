46af06001b48f5e6e8c11782ee736d1f39832d6f
Maximilien Pereira <maxperei@z2r3p6.42lyon.fr>
2023-06-25 12:26:42 +0200

    Add file structure

---
+++ includes/main.hpp
@@ -0,0 +1,25 @@
+#ifndef MAIN_HPP
+# define MAIN_HPP
+
+# include <iostream>
+# include <vector>
+# include <map>
+# include <list>
+# include <fstream>
+# include <string>
+# include <cmath>
+# include <ctime>
+# include <unistd.h>
+
+// UTILS
+# define LIVES 6
+
+// DISPLAY
+# define GREEN "\e[92m"
+# define YELLOW "\e[93m"
+# define RED "\e[91m"
+# define GREY "\e[90m"
+# define RESET "\e[0m"
+# define CLR_LAST_LINE "\e[1A\e[K"
+
+#endif
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,25 +1,5 @@
-
-#include <iostream>
-#include <vector>
-#include <map>
-#include <list>
-#include <fstream>
-#include <string>
-#include <cmath>
-#include <ctime>
-#include <unistd.h>
-
-#include "Letter.hpp"
-
-#define LIVES 6
-#define GREEN "\e[92m"
-#define YELLOW "\e[93m"
-#define RED "\e[91m"
-#define GREY "\e[90m"
-#define RESET "\e[0m"
-#define CLR_LAST_LINE "\e[1A\e[K"
-
-
+#include "../includes/main.hpp"
+#include "../includes/Letter.hpp"
 
 bool isWordKnown(std::vector<std::string> &refList, const std::string &word)
 {

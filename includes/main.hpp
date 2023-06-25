550c4edfce1fdbef102d60b7d865a89dec1a8abb
Maximilien Pereira <maxperei@z2r3p6.42lyon.fr>
2023-06-25 15:18:43 +0200

    Add parsing and welcome functions

--- includes/main.hpp
+++ includes/main.hpp
@@ -10,10 +10,13 @@
 # include <cmath>
 # include <ctime>
 # include <unistd.h>
+# include <sys/stat.h>
 
 // UTILS
 # define LIVES 6
 # define WORD_SIZE 5
+# define MIN_DICT_SIZE 100
+# define MAX_DICT_SIZE 100000
 
 // DISPLAY
 # define GREEN "\e[92m"
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,6 +1,41 @@
 #include "../includes/main.hpp"
 #include "../includes/Letter.hpp"
 
+void	printWelcome(void)
+{
+	std::string	welcomeStr[12] = {
+		"#####################################################################################",
+		"#                                                                                   #",
+		"#         **       **    *******    *******    *******    **        ********        #",
+		"#        /**      /**   **/////**  /**////**  /**////**  /**       /**/////         #",
+		"#        /**   *  /**  **     //** /**   /**  /**    /** /**       /**              #",
+		"#        /**  *** /** /**      /** /*******   /**    /** /**       /*******         #",
+		"#        /** **/**/** /**      /** /**///**   /**    /** /**       /**////          #",
+		"#        /**** //**** //**     **  /**  //**  /**    **  /**       /**              #",
+		"#        /**/   ///**  //*******   /**   //** /*******   /******** /********        #",
+		"#        //       //    ///////    //     //  ///////    ////////  ////////         #",
+		"#                                                                                   #",
+		"#####################################################################################"
+	};
+	for (int i = 0; i < 12; i++)
+		std::cout << welcomeStr[i] << std::endl;
+}
+
+void	printRules(unsigned int dictSize)
+{
+	std::cout << std::endl << std::endl;
+	std::cout << "RULES OF THE GAME:" << std::endl << std::endl;
+	std::cout << "1#   6 guesses to find the WORDLE" << std::endl;
+	std::cout << "2#   you can only enter five letter WORDLES" << std::endl;
+	std::cout << "3#   the letters can only be lowercase" << std::endl;
+	std::cout << "4#   a GREEN letter means it is valid and at the right place in the WORDLE" << std::endl;
+	std::cout << "5#   a YELLOW letter means it is valid but not at the right place in the WORDLE" << std::endl;
+	std::cout << "6#   a GREY letter means it is not valid for the WORDLE" << std::endl;
+	std::cout << std::endl << "7#   find the WORDLE" << std::endl;
+	std::cout << std::endl << "Today the pool of WORDLE size is: " + std::to_string(dictSize) << std::endl;
+	std::cout << std::endl << std::endl;
+}
+
 bool isWordKnown(std::vector<std::string> &refList, const std::string &word)
 {
     for (const auto &item: refList) {
@@ -111,14 +146,48 @@ void checkLetters(const std::string &pword, const std::string &target) {
     std::cout << ")" RESET;
 }
 
+bool	closeError(std::ifstream *inputStream, std::string msg)
+{
+	inputStream->close();
+	std::cerr << msg << std::endl;
+    return false;
+}
+
 bool    parseDict(std::ifstream *is, std::vector<std::string> &dic)
 {
     std::string line;
+    struct stat fileStat;
+    size_t      dictSize = 0;
 
     if (!is->is_open())
+    {
+        std::cerr << "Error: Could not open dictionnary" << std::endl;
         return (false);
+    }
+    else if (!(stat("./words.txt", &fileStat) == 0 && S_ISREG(fileStat.st_mode)))
+        return (closeError(is, "Error: ./words.txt is not a regular file"));
     while(std::getline(*is, line))
+    {
+        dictSize++;
+        if (is->fail())
+			return (closeError(is, "Error: fail while reading"));
+		else if (dictSize > MAX_DICT_SIZE)
+			return (closeError(is, "Error: dictionnary has more than " + std::to_string(MAX_DICT_SIZE) + " entries"));
+		else if (line.size() != 5)
+			return (closeError(is, "Error: dictionnary entry " + std::to_string(dictSize) + " does not have a length of " + std::to_string(WORD_SIZE)));
+		for (unsigned int i = 0; i < line.size(); i++)
+		{
+            line[i] = std::tolower(line[i]);
+			if (!islower(line[i]))
+				return closeError(is, "Error: dictionnary entry " + std::to_string(dictSize) + " does not contain only alphabetic characters");
+		}
         dic.push_back(line);
+    }
+    if (dictSize < MIN_DICT_SIZE)
+        return (closeError(is, "Error: dictionnary has less than " + std::to_string(MIN_DICT_SIZE) + " entries"));
+    else if (!is->eof() && !is->good())
+		return (closeError(is, "Error: error while reading dictionnary file"));
+    is->close();
     return (true);
 }
 
